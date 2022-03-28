e229afa04d4ee049855c77a340527b5dcfeb5c67
LoganPaulus <82315449+LoganPaulus@users.noreply.github.com>
2022-03-28 16:56:42 +0300

    Actually the second commit, but whatever
    aaaaaaaaaaaaaaaa

---
+++ config.lua
@@ -0,0 +1,11 @@
+-- It sends a embed, because who doesnt like embeds lmao
+-- simply fill this config, most of this stuff is also optional.
+-- all the optional shit you can just leave blank
+messageTitle = ""  -- message title yk (optional)
+messageUrl = "" -- Hyperlinks the title, basically just put your server join link or something here(optional)
+message  = "" -- message itself 
+footermessage = "" -- Message footer (optional)
+webhookLink = "" -- REQUIRED 
+color = "" -- It has to be a decimal, example: 390768 is blue. Just google deciaml colors or something
+webhookName = "" -- Webhooks username, displayed on discord
+webhookAvatar = "" -- webhooks avatar, that is displayed on discord. Example: https://cdn.discordapp.com/attachments/858398112142065715/957988112955494480/IMG_0349.png
---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Swaglord69420#6969'
+description 'Sends a message when server is ONLINE'
+version '2.0'
+
+server_script 'config.lua'
+server_script 'server.lua'
